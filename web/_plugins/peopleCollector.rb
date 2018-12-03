36f08e04617ae477487878db01ee71ac02f14411
thesave <thesave86@gmail.com>
2018-12-03 16:13:43 +0100

    created visiting members section

--- web/_plugins/peopleCollector.rb
+++ web/_plugins/peopleCollector.rb
@@ -1,3 +1,5 @@
+require "pp"
+
 module Jekyll
   class PeopleCollector < Generator
     priority :highest
@@ -8,7 +10,13 @@ module Jekyll
       Dir["#{base}/people/*.json"].sort_by{ |f| File.basename( f , ".json" ) }.each do | f |
         people << JSON.parse( File.read( f ) )
       end
+      visiting = Array.new
+      Dir["#{base}/people/visiting/*.json"].sort_by{ |f| File.basename( f , ".json" ) }.each do | f |
+        visiting << JSON.parse( File.read( f ) )
+      end
       site.data["people"] = people
+      site.data["visiting"] = visiting
+      pp site.data
     end
   end
 end
