1b2a2a922736a61f99c904ac4d2b1d50cc3918e2
Marco Peressotti <mperessotti@users.noreply.github.com>
2020-07-08 15:37:19 +0200

    bug in papersGenerator

--- web/_plugins/papersGenerator.rb
+++ web/_plugins/papersGenerator.rb
@@ -104,11 +104,12 @@ module Jekyll
           b[0].delete("url")
           b[0].title = "<span class=\"paper-title\">#{b[0].title}</span>"
           cp = CiteProc::Processor.new style: 'apa', format: 'text'
-          name = cp.engine.style.macros['author'] > 'names' > 'name'
-          name[:initialize] = 'false'
-          name["sort-separator"] = ' '
-          name["form"] = 'long'
-          name["name-as-sort-order"] = 'active'
+          # BROKEN
+          # name = cp.engine.style.macros['author'] > 'names' > 'name'
+          # name[:initialize] = 'false'
+          # name["sort-separator"] = ' '
+          # name["form"] = 'long'
+          # name["name-as-sort-order"] = 'active'
 
           item["id"] = b[0].key
 
